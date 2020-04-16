a6d97b3db0fbf9572acb748e1a119c6205a9b8c2
Евгений Суворов <terrokt@outlook.com>
2020-04-16 10:48:04 +0300

    sdfgkds

---
+++ module1/task1/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: "airbnb-base",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    "no-console": "off"
+  }
+};
